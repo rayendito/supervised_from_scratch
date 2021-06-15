c842668ae2caed87d3301b1200886068189af4fd
rayendito <ryanditod@gmail.com>
2021-06-15 22:05:35 +0700

    KNN done

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import csv
-# from src.KNN import printhello
+from math import floor
+from src.KNN import *
 
 #open file
-csvfile = open("heart.csv", encoding="UTF-8")
-csvread = csv.reader(csvfile, delimiter=',')
+read = list(csv.reader(open("heart.csv", mode='r', encoding='utf-8-sig')))
+column = read[:1]
+data = read[1:]
 
-# print(csvread.line_num)
-for row in csvread:
-    print(len(row))
-    break
+#how many to be tested and K value
+n = 8
+K = 5
+
+#create dictionary from column name
+columnIndex = {}
+for i in range (len(column[0])):
+    columnIndex[column[0][i]] = i
+
+#split to predict the first 5 entries as if unlabeled
+#ntar biar bisa liat akurasinya
+test = data[:n]
+train = data[n:]
+
+accuracy = 0
+for t in test:
+    if(KNN(t, train, columnIndex['target'], K)):
+        accuracy += 1
+    print()
+
+print("accuracy is",(float(accuracy)/n*100),"%")
+
+# print(data[0])
+# print(columnIndex)
+# print(arraydata[0])
--- src/KNN.py
+++ src/KNN.py
@@ -5,10 +5,34 @@ def calculateEucDistance(row1, row2, labelIndex):
     dist = 0
     for i in range(len(row1)):
         if (i != labelIndex):
-            dist += (row1[i]-row2[i])**2
+            dist += (float(row1[i])-float(row2[i]))**2
     return sqrt(dist)
 
-def KNN(existingSet, toPredict, labelIndex, K):
+def KNN(toPredict, train, labelIndex, K):
     neighbors = []
-    for row in existingSet:
-        
+    for i in train:
+        neighbors.append((round(calculateEucDistance(toPredict,i,labelIndex),2), i[labelIndex]))
+    
+    #sort based on the lowest value
+    neighbors.sort(key=lambda x:x[0])
+
+    #counting neighbor categories within K
+    zero = 0
+    one = 0
+    for i in range(K):
+        if(int(neighbors[i][1]) == 0):
+            zero += 1
+        else:
+            one += 1
+    
+    #deciding
+    if(zero > one):
+        prediction = 0
+    else:
+        prediction = 1
+    
+    print("predicted result is",prediction)
+    print("while it's actually",toPredict[labelIndex])
+    print("nearest neighbors are",neighbors[:K])
+    return prediction == int(toPredict[labelIndex])
+
