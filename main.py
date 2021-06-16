227f209b6ba13a2fa64bab14b01e2ba6371ed3dd
rayendito <ryanditod@gmail.com>
2021-06-17 01:52:35 +0700

    log res update
    something is definitely wrong but it's 2am. but still, some improvements tho lmao

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import csv
+import random
 from math import floor
 from src.KNN import *
+from src.logistic_regression import *
 
 #open file
 read = list(csv.reader(open("heart.csv", mode='r', encoding='utf-8-sig')))
 column = read[:1]
 data = read[1:]
+random.shuffle(data)
 
 #create dictionary from column name
 columnIndex = {}
 for i in range (len(column[0])):
     columnIndex[column[0][i]] = i
 
-def driverKNN(n, K, target, columnIndex):
-    #split to predict the first 5 entries as if unlabeled
+def driverKNN(n, K, target):
+    #split to predict the first n entries as if unlabeled
     #ntar biar bisa liat akurasinya
     test = data[:n]
     train = data[n:]
@@ -26,5 +29,25 @@ def driverKNN(n, K, target, columnIndex):
 
     print("accuracy is",(float(accuracy)/n*100),"%")
 
+def driverLogReg(lr, epoch, target):
+    #set treshold
+    treshold1 = 0.5
+
+    #test 10 aja
+    test = data[:10]
+    train = data[10:]
+    coef = train_logistic_regression(train, columnIndex[target], lr, epoch)
+
+    accuracy = 0
+    for i in test:
+        print(log_reg_predict(coef, i, columnIndex[target]))
+        if(log_reg_predict(coef, i, columnIndex[target]) >= treshold1):
+            print("prediction :",1,"while actual value : ",i[13])
+        else:
+            print("prediction :",0,"while actual value : ",i[13])
+
+
+
 #jalanin
-driverKNN(8, 5, 'target', columnIndex)
+# driverKNN(8, 5, 'target')
+driverLogReg(0.01, 5, 'target')
--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -4,8 +4,8 @@ from random import randint
 def train_logistic_regression(train, labelIdx, lr, epochs):
     #set initial values for coefficients = 0
     #coefficients[0] is b0 and setting labelIdx to -1 bc it doesnt have a coefficient
-    coefficients = [0.0 for i in range (len(train[0])+1)]
-    coefficients[labelIdx] = -1
+    coefficients = [0 for i in range (len(train[0])+1)]
+    coefficients[labelIdx+1] = -1
 
     #train n times, according to epochs value
     for i in range(epochs):
@@ -14,22 +14,24 @@ def train_logistic_regression(train, labelIdx, lr, epochs):
 
         #fit and refine
         prediction = log_reg_predict(coefficients, stoc, labelIdx)
-        refineCoefficient(coefficients, prediction, labelIdx)
+        refineCoefficient(coefficients, prediction, stoc, labelIdx, lr)
+        print("coefficients are now :")
+        print(coefficients)
     
     #function returns final coefficients
     return coefficients
 
 def refineCoefficient(coeff, prediction, testEntry, labelIdx, lr):
     for i in range(len(coeff)):
-        if(coeff[i] != -1):
+        if(i != labelIdx+1):
             if(i == 0):
-                coeff[i] += lr * (testEntry[labelIdx]-prediction) * prediction * (1 - prediction)
+                coeff[i] += lr * (float(testEntry[labelIdx])-prediction) * prediction * (1 - prediction)
             else:
-                coeff[i] += lr * (testEntry[labelIdx]-prediction) * prediction * (1 - prediction) * testEntry[i]
+                coeff[i] += lr * (float(testEntry[labelIdx])-prediction) * prediction * (1 - prediction) * float(testEntry[i-1])
 
 def log_reg_predict(coeff, entry, labelIdx):
     topower = coeff[0]
     for i in range (1,len(coeff)):
-        if(coeff[i] != -1):
-            topower += coeff[i]*float(entry[i])
-    return 1/(1 + math.exp(-topower))
+        if(i != labelIdx+1):
+            topower += coeff[i]*float(entry[i-1])
+    return float(1/(1 + math.exp(-topower)))
