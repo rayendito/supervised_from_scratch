15a0ca0144b43287f52e7d8045472a91d14eaf02
rayendito <ryanditod@gmail.com>
2021-07-02 10:06:10 +0700

    read using pandas, select attributes manually

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import csv
 import random
+import pandas as pd
 import numpy as np
 from math import floor
 from src.KNN import *
 from src.logistic_regression import *
 
-#open file
-read = list(csv.reader(open("heart.csv", mode='r', encoding='utf-8-sig')))
-column = read[:1]
-data = np.array(read[1:])
-data = data.astype(np.float64)
+#open file using pandas
+df = pd.read_csv (r'heart.csv')
+
+#input kolom yang bakal dimasukin
+koloms = [item for item in input("Masukkan kolom2 atribut : ").split()]
+target = input("Masukkan kolom target : ")
+toProcess = df[koloms]
+data = toProcess.values.tolist()
 np.random.shuffle(data)
+'''
+*with target (0/1)
+
+For categorical Attributes :
+sex cp fbs restcg exang slope ca thal target
+
+For non-categorical Attributes :
+age trestbps chol thalach oldpeak target
+'''
 
 #create dictionary from column name
 columnIndex = {}
-for i in range (len(column[0])):
-    columnIndex[column[0][i]] = i
+column = list(toProcess.columns)
+for i in range (len(column)):
+    columnIndex[column[i]] = i
 
 def driverKNN(n, K, target):
     #split to predict the first n entries as if unlabeled
@@ -52,7 +66,6 @@ def driverLogReg(lr, epoch, target):
             print("expected :",i[columnIndex[target]],"predicted :",pred,"[0]")
 
 
-
 #jalanin
-# driverKNN(8, 5, 'target')
-driverLogReg(0.001, 10, 'target')
+driverKNN(8, 5, target)
+# driverLogReg(0.001, 10, target)
